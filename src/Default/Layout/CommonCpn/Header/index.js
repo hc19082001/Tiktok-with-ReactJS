6b00795d067e547c440d4d1bdefddd69621b8b37
Nguyễn Huy Cường <lqm1908@gmail.com>
2022-08-15 22:33:48 +0700

    Logo + Searchbar

--- src/Default/Layout/CommonCpn/Header/index.js
+++ src/Default/Layout/CommonCpn/Header/index.js
@@ -1,5 +1,12 @@
 import styles from "./Header.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faCircleXmark,
+    faSpinner,
+    faMagnifyingGlass
+} from "@fortawesome/free-solid-svg-icons";
 import classNames from "classnames/bind";
+import logo from "../../../../assets/image/logo.svg";
 
 const cn = classNames.bind(styles);
 
@@ -7,7 +14,22 @@ function Header() {
     return (
         <header className={cn("header")}>
             <div className={cn("content")}>
-                <h1>Header</h1>
+                <img src={logo} alt="Tiktok" />
+                <div className={cn("search")}>
+                    <input
+                        placeholder="Search accounts and videos"
+                        spellCheck={false}
+                    />
+                    <button className={cn("clear-btn")}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cn("load")} icon={faSpinner} />
+                    <button className={cn("search-btn")}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                <div className={cn("actions")}></div>
             </div>
         </header>
     );
